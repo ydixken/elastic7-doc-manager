ba856d664b67d421bf79ca7dbe0e74fa2b168126
Tharun Mathew Paul <tharun@nineleaps.com>
2019-09-16 12:52:34 +0530

    Change name in package version

--- mongo_connector/doc_managers/elastic7_doc_manager.py
+++ mongo_connector/doc_managers/elastic7_doc_manager.py
@@ -72,7 +72,7 @@ DEFAULT_SEND_INTERVAL = 5
 
 DEFAULT_AWS_REGION = "us-east-1"
 
-__version__ = importlib_metadata.version("elastic2_doc_manager")
+__version__ = importlib_metadata.version("elastic7_doc_manager")
 
 
 def convert_aws_args(aws_args):
